34d00fb70ae437ea0a01587df45508eefa57bf14
danya-pixel <exisionmonkey@gmail.com>
2024-08-26 07:26:43 +0000

    ctc decode + beam search

--- hw_asr/tests/test_text_encoder.py
+++ hw_asr/tests/test_text_encoder.py
@@ -1,4 +1,5 @@
 import unittest
+import torch
 
 from hw_asr.text_encoder.ctc_char_text_encoder import CTCCharTextEncoder
 
@@ -13,6 +14,51 @@ class TestTextEncoder(unittest.TestCase):
         decoded_text = text_encoder.ctc_decode(inds)
         self.assertIn(decoded_text, true_text)
 
-    def test_beam_search(self):
-        # TODO: (optional) write tests for beam search
-        pass
+    def test_beam_search_empty_input(self):
+        text_encoder = CTCCharTextEncoder()
+        probs = torch.tensor([])
+        probs_length = 0
+        beam_size = 10
+        hypos = text_encoder.ctc_beam_search(probs, probs_length, beam_size)
+        self.assertEqual(len(hypos), 0)
+
+    def test_beam_search_single_char_input(self):
+        text_encoder = CTCCharTextEncoder()
+        voc_size = len(text_encoder.ind2char)
+        probs = torch.tensor([[0.0] * voc_size])  
+        probs[0, text_encoder.char2ind['d']] = 1.0  
+        probs_length = 1
+        beam_size = 1
+        hypos = text_encoder.ctc_beam_search(probs, probs_length, beam_size)
+        self.assertEqual(len(hypos), 1)
+        self.assertEqual(hypos[0].text, "d")
+
+    def test_beam_search_multi_char_input(self):
+        text_encoder = CTCCharTextEncoder()
+        voc_size = len(text_encoder.ind2char)
+        probs = torch.tensor([[0.0] * voc_size, [0.0] * voc_size, [0.0] * voc_size]) 
+        probs[0, text_encoder.char2ind['d']] = 1.0  
+        probs[1, text_encoder.char2ind['d']] = 1.0  
+        probs[2, text_encoder.char2ind['d']] = 1.0  
+        probs_length = 3
+        beam_size = 2
+        hypos = text_encoder.ctc_beam_search(probs, probs_length, beam_size)
+        self.assertEqual(len(hypos), 2)
+        self.assertIn("ddd", [hypo.text for hypo in hypos])
+
+    def test_beam_search_beam_size(self):
+        text_encoder = CTCCharTextEncoder()
+        voc_size = len(text_encoder.ind2char)
+        probs = torch.tensor([[0.0] * voc_size, [0.0] * voc_size, [0.0] * voc_size])  
+        probs[0, text_encoder.char2ind['d']] = 1.0 
+        probs[1, text_encoder.char2ind['d']] = 1.0  
+        probs[2, text_encoder.char2ind['d']] = 1.0 
+        probs_length = 3
+        beam_size = 1
+        hypos = text_encoder.ctc_beam_search(probs, probs_length, beam_size)
+        self.assertEqual(len(hypos), 1)
+        self.assertEqual(hypos[0].text, "ddd")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -20,18 +20,45 @@ class CTCCharTextEncoder(CharTextEncoder):
         self.char2ind = {v: k for k, v in self.ind2char.items()}
 
     def ctc_decode(self, inds: List[int]) -> str:
-        # TODO: your code here
-        raise NotImplementedError()
+        if not inds:
+            return ""
+        
+        chars = [self.ind2char[ind] for ind in inds]
 
-    def ctc_beam_search(self, probs: torch.tensor, probs_length,
+        result = []
+        prev_char = None
+        for char in chars:
+            if char != self.EMPTY_TOK and char != prev_char:
+                result.append(char)
+            prev_char = char
+
+        return ''.join(result)
+    
+    def ctc_beam_search(self, probs: torch.tensor, probs_length: int,
                         beam_size: int = 100) -> List[Hypothesis]:
         """
         Performs beam search and returns a list of pairs (hypothesis, hypothesis probability).
         """
+  
+        if probs.numel() == 0:
+            return []
+        
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
         hypos: List[Hypothesis] = []
-        # TODO: your code here
-        raise NotImplementedError
+        
+        hypos = [Hypothesis("", 1.0)]
+
+        for step in range(probs_length):
+            new_hypos = []
+            for hypo in hypos:
+                for next_char_idx in range(voc_size):
+                    next_char = self.ind2char[next_char_idx]
+                    next_prob = probs[step, next_char_idx]
+                    new_text = hypo.text + next_char
+                    new_prob = hypo.prob * next_prob
+                    new_hypos.append(Hypothesis(new_text, new_prob))
+            hypos = sorted(new_hypos, key=lambda x: x.prob, reverse=True)[:beam_size]
+
         return sorted(hypos, key=lambda x: x.prob, reverse=True)
